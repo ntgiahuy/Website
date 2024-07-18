73df15edefc5b080ac149d272efd94cea618b410
Gia Huy <118108313+ntgiahuy@users.noreply.github.com>
2024-07-18 13:32:09 +0700

    Update like.js

--- js/like.js
+++ js/like.js
@@ -1,95 +1,112 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js';
-import { getDatabase, ref, get, set } from 'https://www.gstatic.com/firebasejs/9.11.0/firebase-database.js';
+if (ghLikeFbase.sharedBy === 'www.giahuy.net') {
+    function intToString(value) {
+        const thresholds = [
+            { value: 1e3, suffix: 'K' },
+            { value: 1e6, suffix: 'M' },
+            { value: 1e9, suffix: 'B' },
+            { value: 1e12, suffix: 'T' },
+            { value: 1e15, suffix: 'P' },
+            { value: 1e18, suffix: 'E' }
+        ];
 
-const ghLikeFbase = {
-    firebaseUrl: 'https://like-giahuy-default-rtdb.firebaseio.com/',
-    abbreviation: '2',  // 0 or 1 or 2
-    sharedBy: 'www.giahuy.net' // Credit do not remove
-};
+        value = value.toString().replace(/[^0-9.]/g, '');
+        if (value < 1000) return value;
 
-function intToString(value) {
-    const thresholds = [
-        { value: 1e3, suffix: 'K' },
-        { value: 1e6, suffix: 'M' },
-        { value: 1e9, suffix: 'B' },
-        { value: 1e12, suffix: 'T' },
-        { value: 1e15, suffix: 'P' },
-        { value: 1e18, suffix: 'E' }
-    ];
-
-    value = value.toString().replace(/[^0-9.]/g, '');
-    if (value < 1000) return value;
-
-    for (let i = thresholds.length - 1; i >= 0; i--) {
-        if (value >= thresholds[i].value) {
-            return (value / thresholds[i].value).toFixed(2).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, '$1') + thresholds[i].suffix;
+        for (let i = thresholds.length - 1; i >= 0; i--) {
+            if (value >= thresholds[i].value) {
+                return (value / thresholds[i].value).toFixed(2).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, '$1') + thresholds[i].suffix;
+            }
         }
     }
-}
 
-const ghLikePostId = document.querySelector('.gh-like-btn').getAttribute('data-like').split('/')[1];
+    const ghLikePostId = document.querySelector('.gh-like-btn').getAttribute('data-like').split('/')[1];
 
-function getLiked() {
-    const elementId = '#gh-like';
-    const attribute = 'data-click';
+    function getLiked() {
+        const elementId = '#gh-like';
+        const attribute = 'data-click';
 
-    const db = getDatabase();
-    const likeRef = ref(db, document.querySelector('.gh-like-btn').getAttribute('data-like'));
+        const firebaseUrl = `${ghLikeFbase.firebaseUrl}${document.querySelector('.gh-like-btn').getAttribute('data-like')}`;
+        const dbRef = firebase.database().ref(firebaseUrl);
 
-    get(likeRef).then((snapshot) => {
-        let likeCount = snapshot.val() || 0;
-        set(likeRef, likeCount);
+        dbRef.once('value').then(snapshot => {
+            let likeCount = snapshot.val() || 0;
+            dbRef.set(likeCount);
 
-        if (ghLikeFbase.abbreviation === '0') {
-            document.querySelector(elementId).setAttribute(attribute, likeCount);
-        } else if (ghLikeFbase.abbreviation === '1') {
-            document.querySelector(elementId).setAttribute(attribute, intToString(likeCount));
-        } else if (ghLikeFbase.abbreviation === '2') {
-            document.querySelector(elementId).setAttribute(attribute, likeCount.toLocaleString('id-ID'));
-        }
-    });
-}
+            if (ghLikeFbase.abbreviation === '0') {
+                document.querySelector(elementId).setAttribute(attribute, likeCount);
+            } else if (ghLikeFbase.abbreviation === '1') {
+                document.querySelector(elementId).setAttribute(attribute, intToString(likeCount));
+            } else if (ghLikeFbase.abbreviation === '2') {
+                document.querySelector(elementId).setAttribute(attribute, likeCount.toLocaleString('id-ID'));
+            }
+        });
+    }
 
-function ghLike() {
-    const db = getDatabase();
-    const likeRef = ref(db, document.querySelector('.gh-like-btn').getAttribute('data-like'));
+    function ghLike() {
+        const firebaseUrl = `${ghLikeFbase.firebaseUrl}${document.querySelector('.gh-like-btn').getAttribute('data-like')}`;
+        const dbRef = firebase.database().ref(firebaseUrl);
 
-    get(likeRef).then((snapshot) => {
-        let likeCount = snapshot.val() || 0;
+        dbRef.once('value').then(snapshot => {
+            let likeCount = snapshot.val() || 0;
 
-        if (localStorage.getItem(`ghLike${ghLikePostId}`) !== ghLikePostId) {
-            likeCount++;
-            localStorage.setItem(`ghLike${ghLikePostId}`, ghLikePostId);
-            document.querySelector('.gh-like-btn svg').classList.add('like');
-            document.querySelector('#gh-like').setAttribute('data-text', document.querySelector('#gh-like').getAttribute('data-after'));
-        }
+            if (localStorage.getItem(`ghLike${ghLikePostId}`) !== ghLikePostId) {
+                likeCount++;
+                localStorage.setItem(`ghLike${ghLikePostId}`, ghLikePostId);
+                document.querySelector('.gh-like-btn svg').classList.add('like');
+                document.querySelector('#gh-like').setAttribute('data-text', document.querySelector('#gh-like').getAttribute('data-after'));
+            }
 
-        set(likeRef, likeCount);
+            dbRef.set(likeCount);
 
-        if (ghLikeFbase.abbreviation === '0') {
-            document.querySelector('#gh-like').setAttribute('data-click', likeCount);
-        } else if (ghLikeFbase.abbreviation === '1') {
-            document.querySelector('#gh-like').setAttribute('data-click', intToString(likeCount));
-        } else if (ghLikeFbase.abbreviation === '2') {
-            document.querySelector('#gh-like').setAttribute('data-click', likeCount.toLocaleString('id-ID'));
-        }
-    });
-}
+            if (ghLikeFbase.abbreviation === '0') {
+                document.querySelector('#gh-like').setAttribute('data-click', likeCount);
+            } else if (ghLikeFbase.abbreviation === '1') {
+                document.querySelector('#gh-like').setAttribute('data-click', intToString(likeCount));
+            } else if (ghLikeFbase.abbreviation === '2') {
+                document.querySelector('#gh-like').setAttribute('data-click', likeCount.toLocaleString('id-ID'));
+            }
+        });
+    }
 
-if (ghLikeFbase.sharedBy === 'www.giahuy.net') {
-    const firebaseConfig = {
-        databaseURL: ghLikeFbase.firebaseUrl
-    };
+    (function() {
+        const scriptUrl = 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+        const databaseUrl = 'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js';
 
-    const app = initializeApp(firebaseConfig);
+        const scriptApp = document.createElement('script');
+        scriptApp.async = true;
+        scriptApp.src = scriptUrl;
+        scriptApp.onload = () => {
+            const scriptDatabase = document.createElement('script');
+            scriptDatabase.async = true;
+            scriptDatabase.src = databaseUrl;
+            scriptDatabase.onload = () => {
+const firebaseConfig = {
+  apiKey: "AIzaSyBST1RkcBdYg-dXj46gyO-T_RNy08BLTLk",
+  authDomain: "web-giahuy.firebaseapp.com",
+  databaseURL: "https://web-giahuy-default-rtdb.firebaseio.com",
+  projectId: "web-giahuy",
+  storageBucket: "web-giahuy.appspot.com",
+  messagingSenderId: "380462958233",
+  appId: "1:380462958233:web:d88495e6a5b967e0a1da08"
+                };
 
-    document.addEventListener('DOMContentLoaded', getLiked);
+                firebase.initializeApp(firebaseConfig);
+                getLiked();
+            };
+
+            document.body.appendChild(scriptDatabase);
+        };
+
+        document.body.appendChild(scriptApp);
+    })();
+
     document.querySelector('.gh-like-btn').addEventListener('click', ghLike);
 
     if (localStorage.getItem(`ghLike${ghLikePostId}`) === ghLikePostId) {
         document.querySelector('.gh-like-btn svg').classList.add('like');
         document.querySelector('#gh-like').setAttribute('data-text', document.querySelector('#gh-like').getAttribute('data-after'));
     }
+
 } else {
     window.location.href = 'https://www.giahuy.net/p/credit.html';
 }
