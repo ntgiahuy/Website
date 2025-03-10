39b326e2b1de78577811624cd048d3ea9ac878de
Gia Huy <118108313+ntgiahuy@users.noreply.github.com>
2025-03-10 21:14:42 +0700

    Update Packer2.js

--- widget/js/packer/Packer2.js
+++ widget/js/packer/Packer2.js
@@ -13,14 +13,24 @@ var WORDS = /\w+/g;
 
 var Packer = Base.extend({
     minify: function(script) {
+        // Ép kiểu script thành chuỗi nếu chưa phải
+        script = String(script);
+        // Loại bỏ ký tự xuống dòng nối (line continuation)
         script = script.replace(Packer.CONTINUE, "");
-        script = Packer.data.exec(script);
-        script = Packer.whitespace.exec(script);
-        script = Packer.clean.exec(script);
+        // Thực hiện loại bỏ hoặc xử lý các dữ liệu nếu cần (theo biểu thức chính quy)
+        // Ở đây dùng replace với regex và nhóm bắt (nếu có) để giữ lại nội dung
+        script = script.replace(Packer.data, "$1");
+        // Rút gọn khoảng trắng
+        script = script.replace(Packer.whitespace, " ");
+        // Xóa comment kiểu /* */ và //...
+        script = script.replace(Packer.clean, "");
         return script;
     },
     
     pack: function(script, base62, shrink) {
+        // Đảm bảo script là chuỗi
+        script = String(script);
+        // Thêm ký tự xuống dòng và thực hiện minify
         script = this.minify(script + "\n");
         if (shrink) script = this._shrinkVariables(script);
         if (base62) script = this._base62Encode(script);    
@@ -33,30 +43,36 @@ var Packer = Base.extend({
             return words.get(word).encoded;
         };
         
-        /* Build the packed script */
-        var p = this._escape(script.replace(WORDS, encode));        
-        var a = Math.min(Math.max(words.size(), 2), 62);
-        var c = words.size();
-        var k = words;
-        var e = Packer["ENCODE" + (a > 10 ? a > 36 ? 62 : 36 : 10)];
-        var r = a > 10 ? "e(c)" : "c";
+        /* Xây dựng mã nén dựa trên từ điển */
+        var p = this._escape(script.replace(WORDS, encode));
+        // i: cơ số (số lượng từ, tối thiểu 2, tối đa 62)
+        var i = Math.min(Math.max(words.size(), 2), 62);
+        // a: tổng số từ được phát hiện
+        var a = words.size();
+        // h: đối tượng từ (Words collection)
+        var h = words;
+        // u: hàm mã hóa dựa trên cơ số (ENCODE10, ENCODE36 hoặc ENCODE62)
+        var u = Packer["ENCODE" + (i > 10 ? i > 36 ? 62 : 36 : 10)];
+        // r: chuỗi thay thế cho các từ (dạng “u(c)” nếu cơ số lớn hơn 10, ngược lại “c”)
+        var r = i > 10 ? "u(c)" : "c";
         
-        return format(Packer.UNPACK, p, a, c, k, e, r);
+        // Gọi hàm format (được định nghĩa bên ngoài) để chèn các giá trị vào mẫu UNPACK
+        return format(Packer.UNPACK, p, i, a, h, u, r);
     },
     
     _escape: function(script) {
-        // Dùng dấu nháy đơn bao quanh chuỗi kết quả, nên escape các ký tự cần thiết
+        // Vì chuỗi cuối cùng được bao bởi dấu nháy đơn nên escape các ký tự \ và '
+        // Đồng thời escape các ký tự xuống dòng để đảm bảo chuỗi nằm trên một dòng
         return script.replace(/([\\'])/g, "\\$1").replace(/[\r\n]+/g, "\\n");
     },
     
     _shrinkVariables: function(script) {
-        // Vì Windows Scripting Host không cho phép regexp.test() trên regex toàn cục,
-        // ta tạo phiên bản toàn cục của các regex cần thiết.
+        // Hàm helper tạo phiên bản toàn cục của RegExp
         var global = function(regexp) {
             return new RegExp(regexp.source, "g");
         };
         
-        var data = []; // Lưu trữ các chuỗi đã encode (string và regex)
+        var data = []; // Mảng lưu trữ các chuỗi (string và regex) đã encode
         var REGEXP = /^[^'"]\//;
         var store = function(string) {
             var replacement = "#" + data.length;
@@ -68,23 +84,22 @@ var Packer = Base.extend({
             return replacement;
         };
         
-        // Base52 encoding (a-Z)
+        // Hàm mã hóa Base52 (sử dụng a-z và A-Z)
         var encode52 = function(c) {
             return (c < 52 ? '' : encode52(parseInt(c / 52))) +
                 ((c = c % 52) > 25 ? String.fromCharCode(c + 39) : String.fromCharCode(c + 97));
         };
                 
-        // Xác định các block, đặc biệt là các block hàm (function blocks) định nghĩa scope
+        // Regex xác định các block của hàm (function blocks)
         var BLOCK = /(function\s*[\w$]*\s*\(\s*([^\)]*)\s*\)\s*)?(\{([^{}]*)\})/;
         var VAR_ = /var\s+/g;
         var VAR_NAME = /var\s+[\w$]+/g;
         var COMMA = /\s*,\s*/;
-        var blocks = []; // Lưu trữ các block chương trình (nội dung giữa dấu {})
+        var blocks = [];
         
-        // Encoder cho các block
+        // Hàm encode cho các block: thay thế các tên biến dài thành tên biến ngắn
         var encode = function(block, func, args) {
             if (func) {
-                // Giải mã block hàm, sau đó sẽ tái phân tích để thu gọn tên biến
                 block = decode(block);
                 var vars = match(block, VAR_NAME).join(",").replace(VAR_, "");
                 var ids = Array2.combine(args.split(COMMA).concat(vars.split(COMMA)));
@@ -108,7 +123,7 @@ var Packer = Base.extend({
             return replacement;
         };
         
-        // Decoder cho các block
+        // Hàm decode phục hồi các block đã encode
         var ENCODED = /~(\d+)~/;
         var decode = function(script) {
             while (ENCODED.test(script)) {
@@ -119,24 +134,19 @@ var Packer = Base.extend({
             return script;
         };
         
-        // Encode chuỗi và regex
-        script = Packer.data.exec(script, store);
-        
+        // Encode các chuỗi và regex bằng cách sử dụng store
+        script = script.replace(Packer.data, store);
         // Loại bỏ closure (dành cho namespace của base2)
         script = script.replace(/new function\(_\)\s*\{/g, "{;#;");
-        
-        // Encode các block, đồng thời thay thế tên biến và tham số
+        // Encode các block, đồng thời thu gọn tên biến trong block
         while (BLOCK.test(script)) {
             script = script.replace(global(BLOCK), encode);
         }
-        
         // Khôi phục các block đã encode
         script = decode(script);
-        
-        // Khôi phục closure (cho base2 namespaces)
+        // Khôi phục lại closure (cho base2 namespaces)
         script = script.replace(/\{;#;/g, "new function(_){");
-        
-        // Khôi phục chuỗi và regex
+        // Khôi phục các chuỗi và regex đã được lưu trong mảng data
         script = script.replace(/#(\d+)/g, function(match, index) {        
             return data[index];
         });
@@ -154,8 +164,7 @@ var Packer = Base.extend({
             "||%6;h=[function(u){return y[u]}];u=function(){return'\\\\w+'};a=1};while(a--)if(h[a])g=g."+
             "replace(new RegExp('\\\\b'+u(a)+'\\\\b','g'),h[a]);return g}('%1',%2,%3,'%4'.split('|'),0,{}))",
     
-    // Các biểu thức chính quy dưới đây dùng để loại bỏ khoảng trắng, comment, ... trong mã
-    data: /([\s\S]*)/,       
+    data: /([\s\S]*)/,
     whitespace: /\s+/g, 
     clean: /\/\*[\s\S]*?\*\/|\/\/.*?(\r?\n)/g
 });
